c719394e9a5bf12fa3e8e5082cc4351f9a997dc7
Bearukun <bearukun@gmail.com>
2016-10-12 12:02:57 +0200

    I win

---
+++ gitrepo/src/gitrepo/Person.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitrepo;
+
+/**
+ *
+ * @author bear
+ */
+public class Person {
+    
+    String nickname = "Benny ze killer";
+
+    @Override
+    public String toString() {
+        return "Person{" + "nickname=" + nickname + '}';
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
