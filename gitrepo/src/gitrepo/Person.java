2e819f46fa5d2fa970ba333118839d3e4032df18
RadeonXRay <ceo_titanic@msn.com>
2016-10-12 12:04:09 +0200

    super string

---
+++ gitrepo/src/gitrepo/Person.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitrepo;
+
+/**
+ *
+ * @author Ceo
+ */
+public class Person {
+    
+    String firstName;
+    String lastName;
+
+    @Override
+    public String toString() {
+        return "This is Sir. :" + lastName + " of " + firstName;
+    }
+    
+    
+    
+}
